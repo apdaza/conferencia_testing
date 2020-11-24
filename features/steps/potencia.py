16b460b8a971086db83e006378a128bc96e9001a
apdaza <apdaza@gmail.com>
2020-11-24 10:46:25 -0500

    agregado steps de potencia

---
+++ features/steps/potencia.py
@@ -0,0 +1,14 @@
+from behave import *
+from calculadora import *
+
+@given('{base:d} y {exponente:d} para calcular la potencia')
+def step_implement(context, base, exponente):
+    pass
+
+@when('la calculadora realiza la potencia')
+def stem_implement(context):
+    pass
+
+@then('el {valor:d} es correcto')
+def step_implement(context, valor):
+    pass
