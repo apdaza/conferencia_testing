948c9119322ccdd61cf400b7179c54b3fae4443c
apdaza <apdaza@gmail.com>
2020-12-02 07:21:32 -0500

    cambiado test unitario de concatenar

--- test_calculator.py
+++ test_calculator.py
@@ -29,5 +29,5 @@ class TestCalculadora:
 
     def test_concatenar(self):
         calculadora = Calculadora()
-        assert "12" == calculadora.concatenar("1", "3")
+        assert "13" == calculadora.concatenar("1", "3")
 
